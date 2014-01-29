fc5825d906864cc43c29a9880deb1ba0d79ca921
Adam D Richman <ar@adamrichman.com>
2014-01-29 11:53:52 -0800

    basic serving of static page and stylesheet on request

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,23 @@
 var http = require("http");
 var handler = require("./request-handler");
+var fs = require("fs");
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+var server = http.createServer(function(req,res){
+  var path = req.url;
+  if (path === '/' || path === '/index.html'){
+    var html = fs.readFileSync('public/index.html', 'utf8');
+    var type = 'text/html';
+  } 
+  if (path === '/styles.css'){
+    var html = fs.readFileSync('public/styles.css', 'utf8');
+    var type = 'text/css';
+  } 
+  res.writeHead(200, {'Content-Type': type});
+  res.end(html);
+  handler.handleRequest;
+});
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
