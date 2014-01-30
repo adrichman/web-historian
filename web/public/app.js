851d0079909dc008fe7ac68ae08a8828f96d5344
Adam D Richman <ar@adamrichman.com>
2014-01-30 12:13:12 -0800

    landing page accepts url and handles it

--- web/public/app.js
+++ web/public/app.js
@@ -4,8 +4,8 @@ $(document).on('ready', function(){
   $('#submitUrlButton').on('click',function(e){
     e.preventDefault();
     var urlToCheck = $('#urlInputField').val();
-    console.log(urlToCheck);
-    
+    window.location.assign(urlToCheck);
+
   });
 
 });
